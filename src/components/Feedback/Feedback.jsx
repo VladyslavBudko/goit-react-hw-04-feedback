29a08d8a5aea5b1b4a215679ab4f98349e7412da
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-10-28 03:36:15 +0300

    добавляет количество при клике

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,17 +1,60 @@
-import React, {Component} from 'react';
+import React from 'react';
+// { Component }
 
 export class Feedback extends React.Component {
+  //   static defaultProps = {
+  //     good: 0,
+  //     neutral: 0,
+  //     bad: 0,
+  //   };
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onBtnClock = btnName => {
+    this.setState(prevState => ({
+        [btnName]: prevState[btnName] + 1,
+    }));
+  };
+
   render() {
     return (
       <>
         <h1>Please leave feedback</h1>
-        <button>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              this.onBtnClock('good');
+            }}
+          >
+            Good
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              this.onBtnClock('neutral');
+            }}
+          >
+            Neutral
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              this.onBtnClock('bad');
+            }}
+          >
+            Bad
+          </button>
+        </div>
         <h2>Statistics</h2>
-        <p>Good:</p>
-        <p>Neutral:</p>
-        <p>Bad:</p>
+        <div>
+          <p>Good:{this.state.good}</p>
+          <p>Neutral:{this.state.neutral}</p>
+          <p>Bad:{this.state.bad}</p>
+        </div>
       </>
     );
   }
